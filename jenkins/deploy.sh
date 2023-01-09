5a061964c41a9207cf97771622503e4039f51475
Hlib Terentiev <nicknema13@gmail.com>
2023-01-09 21:24:04 +0200

    add jenkins

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl create ns jenkins
-kubectl apply -f ./manifests
-kubectl create configmap jenkins-config --from-file=./config/jenkins.yaml --from-file=./config/plugins.txt --namespace jenkins
+microk8s kubectl create ns jenkins
+microk8s kubectl apply -f ./manifests
+microk8s kubectl create configmap jenkins-config --from-file=./config/jenkins.yaml --from-file=./config/plugins.txt --namespace jenkins
